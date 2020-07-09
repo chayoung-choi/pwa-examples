33ca38aa3599136cb50a8b561eb1dac15dfe9878
chayoung_DK <chayoung.dev@gmail.com>
2020-07-09 15:51:23 +0900

    test

--- scripts/index.js
+++ scripts/index.js
@@ -11,29 +11,57 @@
   const addBtn = document.querySelector('#add-button');
   addBtn.style.display = 'none';
 
-  window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
+  window.addEventListener('beforeinstallprompt', function (event) {
+    event.preventDefault();
 
-    addBtn.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      addBtn.style.display = 'none';
-      // Show the prompt
-      deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
-      deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the prompt');
-          } else {
-            console.log('User dismissed the prompt');
-          }
-          deferredPrompt = null;
-        });
-    });
+    window.promptEvent = event;
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      console.log('display-mode is standalone');
+    } else {
+      console.log('show button');
+      addBtn.style.display = 'block';
+    }
   });
+  // window.addEventListener('beforeinstallprompt', (e) => {
+  //   console.log('beforeinstallprompt');
+  //   // Prevent Chrome 67 and earlier from automatically showing the prompt
+  //   e.preventDefault();
+  //   // Stash the event so it can be triggered later.
+  //   deferredPrompt = e;
+  //   // Update UI to notify the user they can add to home screen
+  //   if (window.matchMedia('(display-mode: standalone)').matches) {
+  //     console.log('display-mode is standalone');
+  //   } else {
+  //     console.log('show button');
+  //     addBtn.style.display = 'block';
+  //   }
+  //   // addBtn.addEventListener('click', (e) => {
+  //   //   // hide our user interface that shows our A2HS button
+  //   //   addBtn.style.display = 'none';
+  //   //   // Show the prompt
+  //   //   deferredPrompt.prompt();
+  //   //   // Wait for the user to respond to the prompt
+  //   //   deferredPrompt.userChoice.then((choiceResult) => {
+  //   //       if (choiceResult.outcome === 'accepted') {
+  //   //         console.log('User accepted the prompt');
+  //   //       } else {
+  //   //         console.log('User dismissed the prompt');
+  //   //       }
+  //   //       deferredPrompt = null;
+  //   //     });
+  //   // });
+  // });
+
+  function addToHomeScreen() {
+      addBtn.style.display = 'none';
+      window.promptEvent.prompt();
+      window.promptEvent.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the prompt')
+        } else {
+          console.log('User dismissed the prompt')
+        }
+      })
+  }
 
 })();
